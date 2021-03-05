3e34fc9762317dd76fd2ed8eefa63aa7aaf52c89
Dan <75431018+danbr123@users.noreply.github.com>
2021-03-05 17:34:00 +0200

    -Updated T function to use sorted eigenvectors (by values of the corresponding eigenvalues) -Changed stopping condition in QR_iter from det diff to amax diff (max value in Q-Q_bar@Q should be <=epsilon)

--- spectral_clustering.py
+++ spectral_clustering.py
@@ -37,38 +37,63 @@ def calc_k(eigenval_mat):
     size = eigenval_mat.shape[0]
     max = 0
     k = 0
+    vec_idx = np.argsort(np.diag(eigenval_mat))  # array of the original idx of the sorted eigenvals
     eigvals = np.sort(np.diag(eigenval_mat))
     for i in range(1, int(size/2) + 1):
         eigendiff = abs(eigvals[i] - eigvals[i-1])
         if max < eigendiff:
             max = eigendiff
             k = i
-    return k
+    return k, vec_idx[0:k]
 
 
-def find_t(vec_mat, k):
+def find_t(vec_mat, k, vec_idx_arr):
     size = vec_mat.shape[0]
     T = np.zeros(shape=(size, k))
     for i in range(size):
-        norm_sum = np.sqrt((np.sum(vec_mat[i][0:k]**2)))
-        T[i] = vec_mat[i][0:k]/norm_sum
+        norm_sum = np.sqrt((np.sum(vec_mat[i][vec_idx_arr.tolist()]**2)))
+        T[i] = vec_mat[i][vec_idx_arr.tolist()]/norm_sum
     return T
 
 
 def spectral_clustering(data):
+    start = time.time()
+
     W = array_to_adj_mat2(data)
+
+    end = time.time()
+    print("W:",end - start)
+    start = time.time()
+
     Ln = find_lnorm(W)
+
+    end = time.time()
+    print("Ln:",end - start)
+    start = time.time()
+
     A, Q = mt.QR_iter(Ln)
-    k = calc_k(A)
-    T = find_t(Q,k)
-    # TODO - call normal kmeans from here with T as the data
-    #temporary:
+
+    end = time.time()
+    print("AQ:",end - start)
+    start = time.time()
+
+    k, vec_idx = calc_k(A)
+
+    end = time.time()
+    print("k:",end - start)
+    start = time.time()
+
+    T = find_t(Q, k, vec_idx)
+
+    end = time.time()
+    print("T:",end - start)
     return T
 
 # TESTS ###################################################################
-dataMatrix, y = make_blobs(n_samples=1000, centers=5, n_features=3, random_state = 0)
+dataMatrix, y = make_blobs(n_samples=100, centers=5, n_features=3, random_state = 0)
 
-arr = np.array([[2,1,4],[1,1,1],[2,2,3]], dtype=np.float64)
+#arr = np.array([[2,1,4],[1,1,1],[2,2,3]], dtype=np.float64)
+arr = dataMatrix
 # arr2 = np.array([[ 0.55794805, -0.25573179, -0.30606806],[-0.25573179,  0.53705136, -0.26732382],
 #                 [-0.30606806, -0.26732382,  0.5636308]], dtype=np.float64)
 #
@@ -79,4 +104,3 @@ start = time.time()
 res = spectral_clustering(arr)
 end = time.time()
 print(end - start)
-print(res)
